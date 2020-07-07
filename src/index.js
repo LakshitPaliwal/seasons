a41f611dd2445a05bd82ae4e973c7d2d036d8c37
Lakshit Paliwal <39520168+LakshitPaliwal@users.noreply.github.com>
2020-07-08 02:12:35 +0530

    avoiding conditionals in render

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class App extends React.Component{
 
 constructor(props){
   super(props)
-
+   
   // this.state = { lat:'LOADING....' };
   this.state = { lat:null , errMessage:'' };
   
@@ -19,38 +19,33 @@ constructor(props){
   //     this.setState({errMessage:err.message})
   //   }
   // );
-  }
+  };
    
 componentDidMount(){
   window.navigator.geolocation.getCurrentPosition(
     position => this.setState({lat:position.coords.latitude}),
     err => this.setState({errMessage:err.message})
-    
   );
-}
-  
-
+};
 renderContent(){
   if(this.state.lat && !this.state.errMessage){
     return <div>Latitude : {this.state.lat}
            <SeasonDisplay  lat={this.state.lat} />
     </div> 
    }
-
    if(!this.state.lat && this.state.errMessage){
      return <div>
        Error : {this.state.errMessage}
      </div>
    }
     return <Spinner/>
-}
+};
 render(){
      return (
         <div>
           {this.renderContent()}          
         </div>
      )
-
   }
 }
 ReactDOM.render(
